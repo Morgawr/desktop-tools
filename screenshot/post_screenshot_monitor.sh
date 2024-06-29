5c93a6a954cbcbf260fbefcf426c50f9a8a036e6
Federico 'Morg' Pareschi <morgawr@gmail.com>
2024-06-29 11:43:30 +0900

    Added support for clipboard screenshot monitoring

--- screenshot/post_screenshot_monitor.sh
+++ screenshot/post_screenshot_monitor.sh
@@ -25,3 +25,12 @@ echo "Resized $IN_FILE to $OUT_FILE."
 echo "Removing original file."
 
 rm "$IN_FILE"
+
+echo "Copying to clipboard."
+
+export WAYLAND_DISPLAY=wayland-0
+export XDG_SESSION_DESKTOP=wayland
+export XDG_SESSION_ID=2
+export XDG_RUNTIME_DIR=/run/user/1000
+
+wl-copy --type=image/png < "$OUT_FILE"
